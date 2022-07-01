c60796911e015b992836de5bc9b8491ed72d38ab
Андрей <gosudarev_a@mail.ru>
2022-07-01 16:25:52 +0300

    Ну теперь ошибку не выдает. В предыдущий раз аннотации @Qualifier на раскидал, вот он и понять не мог, куда чего.

--- src/main/java/gos/Andrey1986/_course_project/controller/JavaQuestionController.java
+++ src/main/java/gos/Andrey1986/_course_project/controller/JavaQuestionController.java
@@ -2,6 +2,7 @@ package gos.Andrey1986._course_project.controller;
 
 import gos.Andrey1986._course_project.Question;
 import gos.Andrey1986._course_project.service.QuestionService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,11 @@ import java.util.Collection;
 @RestController
 @RequestMapping ()
 public class JavaQuestionController {
-    QuestionService questionService;
+    @Qualifier ("JavaQuestionService")
+   private final QuestionService questionService;
 
-    public JavaQuestionController(QuestionService questionService) {
-        this.questionService = questionService;
+    public JavaQuestionController(QuestionService javaQuestionService) {
+        this.questionService = javaQuestionService;
     }
 
     @RequestMapping ("/add")
--- src/main/java/gos/Andrey1986/_course_project/controller/MathQuestionController.java
+++ src/main/java/gos/Andrey1986/_course_project/controller/MathQuestionController.java
@@ -1,14 +1,37 @@
 package gos.Andrey1986._course_project.controller;
 
+import gos.Andrey1986._course_project.Question;
 import gos.Andrey1986._course_project.service.QuestionService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/math/")
-public class MathQuestionController extends JavaQuestionController {
+public class MathQuestionController {
+    @Qualifier("MathService")
+    private final QuestionService mQuestionService;
+        public MathQuestionController (@Qualifier("mathService") QuestionService mQuestionService) {
+        this.mQuestionService = mQuestionService;
+    }
 
-    public MathQuestionController(QuestionService mQuestionService) {
-        super(mQuestionService);
+    @RequestMapping()
+    public Collection<Question> getAllQuestions () {
+        return mQuestionService.getAll();
+    }
+
+    @RequestMapping("/add")
+    public Question addQuestion (@RequestParam("question") String question,
+                                 @RequestParam("answer") String answer) {
+        return mQuestionService.add(question, answer);
+    }
+
+    @RequestMapping("/remove")
+    public Question removeQuestion (@RequestParam("question") String question,
+                                    @RequestParam("answer") String answer) {
+        return mQuestionService.remove(new Question(question, answer));
     }
 }
